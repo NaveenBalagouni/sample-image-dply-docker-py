e28da7122cafe165066c815291e761034cd7aad4
BALAGOUNI NAVEEN <balagounishiva678@gmail.com>
2025-01-10 15:02:02 +0530

    edit

--- python-docker-app/app.py
+++ python-docker-app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define the basic route
 @app.route('/')
 def hello_world():
-    return 'Hello, World!.'
+    return 'Hello, World naveen!.'
 
 # Start the server
 if __name__ == '__main__':
